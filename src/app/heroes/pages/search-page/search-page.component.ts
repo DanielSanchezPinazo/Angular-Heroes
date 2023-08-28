e7029792a7c14dca008bad1abd52c5c2dbba74a8
Daniel <danielsanchezpinazo@gmail.com>
2023-08-28 10:29:04 +0200

    Fin sección 14

--- src/app/heroes/pages/search-page/search-page.component.ts
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+
+
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
-  selector: 'app-search-page',
+  selector: 'heroes-search-page',
   templateUrl: './search-page.component.html',
   styles: [
   ]
 })
 export class SearchPageComponent {
+
+  public searchInput = new FormControl( "" );
+
+  public heroes: Hero[] = [];
+
+  public selectedHero?: Hero;
+
+  constructor( private heroesService: HeroesService ) {}
+
+  public searchHero() {
+
+    const value: string = this.searchInput.value || "";
+
+    this.heroesService.getSuggestions( value )
+      .subscribe( heroes => this.heroes = heroes );
+
+    console.log(value);
+
+  }
+
+  public onSelectedOption( event: MatAutocompleteSelectedEvent ): void {
+
+    // console.log(event.option.value);
+
+    if ( !event.option.value ) {
+      this.selectedHero = undefined;
+      return;
+    }
+
+    const hero: Hero = event.option.value;
+
+    this.searchInput.setValue( hero.superhero );
+
+    this.selectedHero = hero;
+
+  }
 
 }
